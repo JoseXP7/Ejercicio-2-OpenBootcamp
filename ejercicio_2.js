2b70a7cb0172949b07c4b477e8edfd190463a6e9
JoseXP7 <98354328+JoseXP7@users.noreply.github.com>
2022-12-18 17:21:34 -0400

    Add files via upload

---
+++ ejercicio_2.js
@@ -0,0 +1,10 @@
+const miPersona = [
+"Jose Manuel", //mi nombre (string)
+18, //mi edad (number)
+true, //soy programador? (boolean)
+new Date(2004, 09, 13), //fecha de nacimiento (date)
+{titulo: "FNAF: The Silver Eyes", //mi libro favorito (object)
+autor: "Scott Cawthon",
+fecha: 2017,
+url: "https://www.worldcat.org/es/title/1336200956"}
+]
